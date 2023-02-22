a21d29459fcb7e8bd66470f73e83a0b89a1b1119
mbugua97 <jobugua97@gmail.com>
2023-02-22 15:35:37 +0300

    functions ,nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<main.h>
+/**
+ * main - print _putchar
+ * is witten by mbugua
+ * Return:0
+ * 
+ */
+int main(int)
+{
+	_putchar('_');
+	_putchar('p');
+	_puchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return(0);
+}
